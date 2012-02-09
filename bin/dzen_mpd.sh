2b5d0aa6c9bd1dbfc51d15678f81299373f0f10d
Lasse Brun <bruners@gmail.com>
2012-02-09 03:06:10 +0100

    Update dzen_mpd with some new colors

--- bin/dzen_mpd.sh
+++ bin/dzen_mpd.sh
@@ -6,15 +6,15 @@
 # with help from Robert Manea
 
 FN='-xos4-terminus-*-*-*-*-12-*-*-*-*-*-*-*'
-BG='#333333'
-FG='#a8a3f7'
+BG='#111'
+FG='white'
 W=640
 H=22
 X=1920
 Y=0
 GH=7
 GW=50
-GFG='#a8a3f7'
+GFG='#aecf96'
 GBG='#111'
 FW="mpc seek +5"      # 5 sec forwards
 RW="mpc seek -5"      # 5 sec backwards
@@ -22,7 +22,7 @@ NEXTS="mpc next"      # previous song
 PREVS="mpc prev"      # next song
 TOGGS="mpc toggle"    # play/pause
 
-CAPTION="^i(/home/lasseb/.share/icons/dzen/play.xbm)"
+CAPTION="^fg(khaki)^i(/home/lasseb/.share/icons/dzen/play.xbm)^fg(white)"
 
 MAXPOS="100"
 
@@ -31,6 +31,6 @@ while true; do
   POSM="$POS $MAXPOS"
   echo -n " $CAPTION "
   echo "`mpc | sed -n '1p'`" | tr '\n' ' '
-  echo "$POSM" | gdbar -h $GH -w $GW -fg $GFG -bg $GBG
+  echo "$POSM" | gdbar -h 10 -w 100 -sw 4 -sh 4 -ss 1 -s h -fg $GFG -bg $GBG 
   sleep 1;
 done #| dzen2 -ta l -tw $W -h $H -x $X -y $Y -fg $FG -bg $BG -fn $FN -e "button4=exec:$RW;button5=exec:$FW;button1=exec:$PREVS;button3=exec:$NEXTS;button2=exec:$TOGGS"
